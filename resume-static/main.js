a1c7d485c98cd5fe589991e49790c3b1c972d163
shahzaib1233 <shahzaibimtiazbt@gmail.com>
2024-09-16 10:25:15 +0500

    update

--- resume-static/main.js
+++ resume-static/main.js
@@ -1,15 +1,14 @@
 var toggleButtons = document.querySelectorAll(".toggleButton");
 var toggleSections = document.querySelectorAll(".toggleSection");
-
-toggleButtons.forEach(function(button, index) {
-    button.addEventListener("click", function() {
+toggleButtons.forEach(function (button, index) {
+    button.addEventListener("click", function () {
         var section = toggleSections[index];
         var sectionStyle = window.getComputedStyle(section);
-
         if (sectionStyle.display === "none") {
             section.style.display = "block";
             button.textContent = "Hide Info";
-        } else {
+        }
+        else {
             section.style.display = "none";
             button.textContent = "Show Info";
         }
